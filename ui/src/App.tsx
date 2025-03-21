a09867bfe6efb2e0dfa78b7ce00b11fc03321e5e
Joey Gold <joey.gold@softwire.com>
2025-03-21 17:45:32 +0000

    Create custom button component example

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.scss'
+import CustomButton from './components/formComponents/customButton/CustomButton'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,9 +19,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <CustomButton onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </CustomButton>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
