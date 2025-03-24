2576ed5a6b1aed8a8b6b2f4c95fa8c89147a8e8b
Joey Gold <joey.gold@softwire.com>
2025-03-24 15:56:01 +0000

    Fix custom button

--- ui/src/components/formComponents/customButton/CustomButton.tsx
+++ ui/src/components/formComponents/customButton/CustomButton.tsx
@@ -1,6 +1,6 @@
 import classNames from "classnames";
 import React from "react";
-import './Button.scss';
+import './CustomButton.scss';
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
