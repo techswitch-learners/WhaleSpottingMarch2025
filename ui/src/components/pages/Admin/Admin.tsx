97fe8d35a5de8767c833d7e1680422eabe2c9913
Siuzanna Abkadyrova <siuzanna.abkadyrova@softwire.com>
2025-03-26 13:53:39 +0000

    Removed demo button

--- ui/src/components/pages/Admin/Admin.tsx
+++ ui/src/components/pages/Admin/Admin.tsx
@@ -1,12 +1,3 @@
-import CustomButton from "../../formComponents/customButton/CustomButton";
-
 export const Admin = () => {
-  return (
-    <div>
-      <div>Admin page</div>
-      <CustomButton>
-        Approve
-      </CustomButton>
-    </div>
-  );
+  return <div>Admin page</div>;
 };
