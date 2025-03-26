aa495c9013845677d561340782b6ba32a6256c5c
Siuzanna Abkadyrova <siuzanna.abkadyrova@softwire.com>
2025-03-26 12:36:56 +0000

    Fixed review comments

--- ui/src/components/pages/Admin/Admin.tsx
+++ ui/src/components/pages/Admin/Admin.tsx
@@ -1,14 +1,11 @@
-import { useState } from "react";
 import CustomButton from "../../formComponents/customButton/CustomButton";
 
 export const Admin = () => {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <div>Admin page</div>
-      <CustomButton onClick={() => setCount((count) => count + 1)}>
-        count is {count}
+      <CustomButton>
+        Approve
       </CustomButton>
     </div>
   );
