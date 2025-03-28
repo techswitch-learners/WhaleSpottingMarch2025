641a1d5b4ec138dfd63484dfb7ca737392598bde
Emine Oraklabel <emine.oraklabel@softwire.com>
2025-03-28 10:54:37 +0000

    #3 - Add SpeciesController

--- api/Repositories/ISpeciesRepository.cs
+++
@@ -1,7 +0,0 @@
-using WhaleSpottingBackend.Models.DatabaseModels;
-namespace WhaleSpottingBackend.Repositories;
-
-public interface ISpeciesRepository
-{
-    Species GetSpeciesByID(int speciesId);
-}
