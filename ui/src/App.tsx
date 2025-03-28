951e27aa2881f16ae2e3833344d74dc6749d13d1
Jamilya Aitakunova <jamilya.aitakunova@softwire.com>
2025-03-28 10:26:40 +0000

    Added Prettier to ESLint Configuration

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,25 +1,24 @@
-import { Routes } from 'react-router'
-import './App.scss'
-import { Route } from 'react-router'
-import { NavigationBar } from './components/NavigationBar/NavigationBar'
-import { Admin } from './components/pages/Admin/Admin'
-import { Landing } from './components/pages/Landing/Landing'
-import { ReportSighting } from './components/pages/ReportSighting/ReportSighting'
-import { ViewSightings } from './components/pages/ViewSightings/ViewSightings'
-
+import { Routes } from "react-router";
+import "./App.scss";
+import { Route } from "react-router";
+import { NavigationBar } from "./components/NavigationBar/NavigationBar";
+import { Admin } from "./components/pages/Admin/Admin";
+import { Landing } from "./components/pages/Landing/Landing";
+import { ReportSighting } from "./components/pages/ReportSighting/ReportSighting";
+import { ViewSightings } from "./components/pages/ViewSightings/ViewSightings";
 
 function App() {
   return (
     <>
       <NavigationBar />
       <Routes>
-        <Route path="/"  element={<Landing />} />
-        <Route path="/ReportSighting"  element={<ReportSighting/>} />
-        <Route path="/ViewSightings"  element={<ViewSightings/>} />
-        <Route path="/Admin"  element={<Admin/>} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/ReportSighting" element={<ReportSighting />} />
+        <Route path="/ViewSightings" element={<ViewSightings />} />
+        <Route path="/Admin" element={<Admin />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.scss'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.scss";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <StrictMode>
       <App />
     </StrictMode>
-  </BrowserRouter>
+  </BrowserRouter>,
 );
