c2bdc741980e1ea79b492ec262ada30958796118
Emine Oraklabel <emine.oraklabel@softwire.com>
2025-03-28 10:50:20 +0000

    Add post endpoint and configure sighting repo

--- api/Repositories/ISightingRepository.cs
+++ api/Repositories/ISightingRepository.cs
@@ -1,7 +1,9 @@
+using WhaleSpottingBackend.Models.ApiModels;
 using WhaleSpottingBackend.Models.DatabaseModels;
 namespace WhaleSpottingBackend.Repositories;
 
 public interface ISightingRepository 
 {
     Sighting GetSightingByID(int sightingId);
+    void PostSighting(CreateSightingRequest sighting);
 }
