021e0c7f1425bd56c45e696385ec68fe82d37848
Preeti Mishra <preeti.mishra@softwire.com>
2025-03-31 16:41:39 +0100

    Ran linter

--- ui/src/components/pages/ReportSighting/ReportSighting.tsx
+++ ui/src/components/pages/ReportSighting/ReportSighting.tsx
@@ -1,8 +1,10 @@
 import { WhaleSightingForm } from "../../formComponents/whaleSightingForm/WhaleSightingForm";
 
 export const ReportSighting = () => {
-  return <div>
-    <h2>Report sighting page</h2>
-    <WhaleSightingForm></WhaleSightingForm>
-  </div>;
+  return (
+    <div>
+      <h2>Report sighting page</h2>
+      <WhaleSightingForm></WhaleSightingForm>
+    </div>
+  );
 };
