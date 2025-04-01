bbf967940059bc8d87b955abcaff1ee0fc731fe6
Jamilya Aitakunova <jamilya.aitakunova@softwire.com>
2025-04-01 13:36:53 +0100

    Started work on Login page

--- ui/src/components/pages/LogIn/LogIn.tsx
+++ ui/src/components/pages/LogIn/LogIn.tsx
@@ -1,3 +1,42 @@
+import React, { FormEvent, useState } from "react";
+import "./LogIn.scss";
+
 export const LogIn = () => {
-  return <div>Log In / Register / Account</div>;
+  //const loginContext = useContext(LoginContext)
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  function tryLogin(event: FormEvent) {
+    event.preventDefault();
+    //loginContext.logIn(username, ,password);
+  }
+  return (
+    <div className="login-container">
+      <h1 className="title">Log In</h1>
+      <form className="login-form" onSubmit={tryLogin}>
+        <label className="form-label">
+          {" "}
+          Username
+          <input
+            className="form-input"
+            type={"text"}
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </label>
+        <label className="form-label">
+          {" "}
+          Password
+          <input
+            className="form-input"
+            type={"password"}
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
+        <button className="submit-button" type="submit">
+          Log In
+        </button>
+      </form>
+    </div>
+  );
 };
