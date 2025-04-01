f2aa2289ee0edbb28a4eba41fa49b90c0844fd4b
Siuzanna Abkadyrova <siuzanna.abkadyrova@softwire.com>
2025-04-01 16:02:35 +0100

    #25 Added Species and Date filters

--- api/Models/ApiModels/SightingsQueryParameters.cs
+++ api/Models/ApiModels/SightingsQueryParameters.cs
@@ -4,4 +4,8 @@ public class SightingsQueryParameters
 {
     public int PageNumber { get; set; } = 1;
     public int PageSize { get; set; } = 10;
+    public int? SpeciesId { get; set; } 
+    public bool? HasImage { get; set; }
+    public DateTime? SightingStartDate { get; set; }
+    public DateTime? SightingEndDate { get; set; } = DateTime.UtcNow;
 }
