760a73e86842e097d92b7b0499532f02b5ab4230
S Chiang <63751262+gosaurus@users.noreply.github.com>
2025-04-02 10:40:37 +0100

    Made imageSource field in CreateSightingRequest nullable; Changed page size

--- api/Models/ApiModels/SightingsQueryParameters.cs
+++ api/Models/ApiModels/SightingsQueryParameters.cs
@@ -3,8 +3,16 @@ namespace WhaleSpottingBackend.Models.ApiModels;
 public class SightingsQueryParameters
 {
     public int PageNumber { get; set; } = 1;
-    public int PageSize { get; set; } = 10;
-    public int? SpeciesId { get; set; } 
+    private int _pageSize = 10;
+    public int PageSize
+    {
+        get
+        { return _pageSize; }
+        set
+        { _pageSize = value < 50 ? _pageSize = value : _pageSize = _maxPageSize; }
+    }
+    private readonly int _maxPageSize = 50;
+    public int? SpeciesId { get; set; }
     public bool? HasImage { get; set; }
     public DateTime? SightingStartDate { get; set; }
     public DateTime? SightingEndDate { get; set; } = DateTime.UtcNow;
