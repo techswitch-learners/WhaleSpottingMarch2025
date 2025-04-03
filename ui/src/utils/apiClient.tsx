860295a760d39c2cf21f87cdcb658a04b3e1b076
S Chiang <63751262+gosaurus@users.noreply.github.com>
2025-04-03 10:57:26 +0100

    Started amending WhaleSightingForm for image upload

--- ui/src/utils/apiClient.tsx
+++ ui/src/utils/apiClient.tsx
@@ -1,15 +1,15 @@
-import { WhaleSighting } from "../components/formComponents/whaleSightingForm/WhaleSightingForm";
+// import { WhaleSighting } from "../components/formComponents/whaleSightingForm/WhaleSightingForm";
 
 export const fetchPOSTRequest = async (
-  formData: WhaleSighting,
+  formData: FormData,
   controllerEndpoint: string,
 ) => {
   const fetchResponse = await fetch(
     import.meta.env.VITE_APP_API_HOST + controllerEndpoint,
     {
       method: "POST",
-      body: JSON.stringify(formData),
-      headers: new Headers({ "Content-Type": "application/json" }),
+      body: formData,
+      // headers: new Headers({ "Content-Type": "application/json" }),
     },
   );
   return fetchResponse.status;
