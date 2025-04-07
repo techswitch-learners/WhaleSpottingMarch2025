432d5c45e62274c5ea357aa02cfbc26a481d2d7a
Preeti Mishra <preeti.mishra@softwire.com>
2025-04-07 14:56:09 +0100

    Deleted Migrations for item #15  and added a fresh migration

---
+++ api/Migrations/20250407135021_AddSightingReview.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace WhaleSpottingBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSightingReview : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SightingReview",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SightingId = table.Column<int>(type: "integer", nullable: false),
+                    AdminId = table.Column<string>(type: "text", nullable: false),
+                    Approved = table.Column<bool>(type: "boolean", nullable: false),
+                    StatusDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Comments = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SightingReview", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SightingReview_AspNetUsers_AdminId",
+                        column: x => x.AdminId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SightingReview_Sighting_SightingId",
+                        column: x => x.SightingId,
+                        principalTable: "Sighting",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SightingReview_AdminId",
+                table: "SightingReview",
+                column: "AdminId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SightingReview_SightingId",
+                table: "SightingReview",
+                column: "SightingId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SightingReview");
+        }
+    }
+}
