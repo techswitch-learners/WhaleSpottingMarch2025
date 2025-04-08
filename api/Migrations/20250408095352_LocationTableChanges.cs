76f630d155dfd91d3e352e658a42c110819a291a
Vaishnavi Rajasekar <vaishnavi.rajasekar@softwire.com>
2025-04-08 10:58:09 +0100

    ran linter and merge conflict resolved

---
+++ api/Migrations/20250408095352_LocationTableChanges.cs
@@ -0,0 +1,198 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NetTopologySuite.Geometries;
+
+#nullable disable
+
+namespace WhaleSpottingBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class LocationTableChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Location");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Location");
+
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:PostgresExtension:postgis", ",,");
+
+            migrationBuilder.AddColumn<Point>(
+                name: "SpatialCoordinates",
+                table: "Location",
+                type: "geometry",
+                nullable: false,
+                defaultValue: new Point(0, 0) { SRID = 4326 });
+
+            migrationBuilder.UpdateData(
+                table: "Location",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "SpatialCoordinates",
+                value: (NetTopologySuite.Geometries.Point)new NetTopologySuite.IO.WKTReader().Read("SRID=4326;POINT (41.9028 -60)"));
+
+            migrationBuilder.UpdateData(
+                table: "Location",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "SpatialCoordinates",
+                value: (NetTopologySuite.Geometries.Point)new NetTopologySuite.IO.WKTReader().Read("SRID=4326;POINT (57.808243 -146.412739)"));
+
+            migrationBuilder.UpdateData(
+                table: "Location",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "SpatialCoordinates",
+                value: (NetTopologySuite.Geometries.Point)new NetTopologySuite.IO.WKTReader().Read("SRID=4326;POINT (34.468535 -130.063914)"));
+
+            migrationBuilder.UpdateData(
+                table: "Location",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "SpatialCoordinates",
+                value: (NetTopologySuite.Geometries.Point)new NetTopologySuite.IO.WKTReader().Read("SRID=4326;POINT (73.149204 -148.475577)"));
+
+            migrationBuilder.UpdateData(
+                table: "Location",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "SpatialCoordinates",
+                value: (NetTopologySuite.Geometries.Point)new NetTopologySuite.IO.WKTReader().Read("SRID=4326;POINT (36.547852 -72.010735)"));
+
+            migrationBuilder.UpdateData(
+                table: "Location",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "SpatialCoordinates",
+                value: (NetTopologySuite.Geometries.Point)new NetTopologySuite.IO.WKTReader().Read("SRID=4326;POINT (52.122217 -50.741203)"));
+
+            migrationBuilder.UpdateData(
+                table: "Location",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "SpatialCoordinates",
+                value: (NetTopologySuite.Geometries.Point)new NetTopologySuite.IO.WKTReader().Read("SRID=4326;POINT (69.169371 -174.813427)"));
+
+            migrationBuilder.UpdateData(
+                table: "Location",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "SpatialCoordinates",
+                value: (NetTopologySuite.Geometries.Point)new NetTopologySuite.IO.WKTReader().Read("SRID=4326;POINT (55.443528 -138.983393)"));
+
+            migrationBuilder.UpdateData(
+                table: "Location",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "SpatialCoordinates",
+                value: (NetTopologySuite.Geometries.Point)new NetTopologySuite.IO.WKTReader().Read("SRID=4326;POINT (44.420668 -56.366203)"));
+
+            migrationBuilder.UpdateData(
+                table: "Location",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "SpatialCoordinates",
+                value: (NetTopologySuite.Geometries.Point)new NetTopologySuite.IO.WKTReader().Read("SRID=4326;POINT (83.249778 -106.815422)"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SpatialCoordinates",
+                table: "Location");
+
+            migrationBuilder.AlterDatabase()
+                .OldAnnotation("Npgsql:PostgresExtension:postgis", ",,");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "Location",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "Location",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.UpdateData(
+                table: "Location",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Latitude", "Longitude" },
+                values: new object[] { 41.902799999999999, -60.0 });
+
+            migrationBuilder.UpdateData(
+                table: "Location",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Latitude", "Longitude" },
+                values: new object[] { 57.808242999999997, -146.41273899999999 });
+
+            migrationBuilder.UpdateData(
+                table: "Location",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Latitude", "Longitude" },
+                values: new object[] { 34.468535000000003, -130.06391400000001 });
+
+            migrationBuilder.UpdateData(
+                table: "Location",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "Latitude", "Longitude" },
+                values: new object[] { 73.149203999999997, -148.47557699999999 });
+
+            migrationBuilder.UpdateData(
+                table: "Location",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "Latitude", "Longitude" },
+                values: new object[] { 36.547851999999999, -72.010734999999997 });
+
+            migrationBuilder.UpdateData(
+                table: "Location",
+                keyColumn: "Id",
+                keyValue: 6,
+                columns: new[] { "Latitude", "Longitude" },
+                values: new object[] { 52.122216999999999, -50.741202999999999 });
+
+            migrationBuilder.UpdateData(
+                table: "Location",
+                keyColumn: "Id",
+                keyValue: 7,
+                columns: new[] { "Latitude", "Longitude" },
+                values: new object[] { 69.169370999999998, -174.81342699999999 });
+
+            migrationBuilder.UpdateData(
+                table: "Location",
+                keyColumn: "Id",
+                keyValue: 8,
+                columns: new[] { "Latitude", "Longitude" },
+                values: new object[] { 55.443528000000001, -138.98339300000001 });
+
+            migrationBuilder.UpdateData(
+                table: "Location",
+                keyColumn: "Id",
+                keyValue: 9,
+                columns: new[] { "Latitude", "Longitude" },
+                values: new object[] { 44.420667999999999, -56.366202999999999 });
+
+            migrationBuilder.UpdateData(
+                table: "Location",
+                keyColumn: "Id",
+                keyValue: 10,
+                columns: new[] { "Latitude", "Longitude" },
+                values: new object[] { 83.249778000000006, -106.815422 });
+        }
+    }
+}
