09e57c874fb13715260de4f7871a68bd42b08d8d
Faith Martin <faith.martin@softwire.com>
2025-04-09 20:22:40 +0100

    "#28 - fetch data from location endpoint in view location"

--- ui/src/components/pages/ViewLocations/ViewLocations.tsx
+++ ui/src/components/pages/ViewLocations/ViewLocations.tsx
@@ -1,3 +1,101 @@
+import { fetchLocationData } from "../../../utils/locationApiClient";
+import { useState, useEffect } from "react";
+import LocationPicker, {
+  GeoLocation,
+} from "../../formComponents/LocationPicker/LocationPicker";
+import { format } from "date-fns";
+import {
+  SightingsResponse,
+  DailyWeatherForecast,
+  TopSpecies,
+  LocationSearchResponse,
+} from "../../../models/apiModels.ts";
+
 export const ViewLocations = () => {
-  return <div>View Locations</div>;
+  const [locationData, setLocationData] = useState<LocationSearchResponse>(
+    null!,
+  );
+  //const [loading, setLoading] = useState(true);
+  const [location, setLocation] = useState<GeoLocation>({
+    latitude: 40.9208,
+    longitude: -60.0,
+  });
+
+  useEffect(() => {
+    const handleLocation = async () => {
+      const data = await fetchLocationData(
+        location.latitude,
+        location.longitude,
+        10,
+      );
+      setLocationData(data);
+      //setLoading(false);
+    };
+
+    if (location.latitude && location.longitude) {
+      handleLocation();
+    }
+  }, [location.latitude, location.longitude]);
+
+  //if (loading) return <p>Loading data</p>;
+  if (!locationData) return <p>No data available for the given location</p>;
+
+  return (
+    <div>
+      <p>View Locations</p>
+      <div className="location-selection">
+        <label className="map-label">Select Sighting Location:</label>
+        <LocationPicker location={location} onLocationSelection={setLocation} />
+      </div>
+      <ul>
+        {locationData.weatherForecast?.map(
+          (day: DailyWeatherForecast, index: number) => (
+            <li key={index}>
+              <p>Day {index + 1}</p>
+              <p> Date: {format(day.date, "dd/MM/yyyy")}</p>
+              <p> Description: {day.description}</p>
+              <p> Min Temperature: {day.minTemperatureInCelcius}&deg;C</p>
+              <p> Max Temperature: {day.maxTemperatureInCelcius}&deg;C</p>
+              <p> Visibility: {day.visibilityInKm} km </p>
+              <p> Max Wind Speed: {day.maxWindSpeedInKmPerHour} km/h </p>
+              <p>
+                Total Precipitation: {day.totalPrecipitationInMilimeters} mm
+              </p>
+            </li>
+          ),
+        )}
+      </ul>
+      <ul>
+        {locationData.topSpecies?.map((species: TopSpecies, index: number) => (
+          <li key={index}>
+            <p>Top Species {index + 1}</p>
+            <p> Species: {species.species}</p>
+            <p> Number of Sightings: {species.numSightings}</p>
+            <p> Last Seen Date: {format(species.lastSeen, "dd/MM/yyyy")} </p>
+          </li>
+        ))}
+      </ul>
+      <ul>
+        {locationData.recentSightings?.map(
+          (sighting: SightingsResponse, index: number) => (
+            <li key={index}>
+              <p>Sighting Id: {sighting.id} </p>
+              <p>Species Id: {sighting.speciesId}</p>
+              <p>Species Name: {sighting.speciesName}</p>
+              <p>Description: {sighting.description}</p>
+              <p>
+                Sighting Date: {format(sighting.sightingDate, "dd/MM/yyyy")}
+              </p>
+              <p>Report Date: {format(sighting.reportDate, "dd/MM/yyyy")}</p>
+              <p>Quantity: {sighting.quantity}</p>
+              <p>
+                Coordinates: {sighting.latitude}, {sighting.longitude}
+              </p>
+              <img src={sighting.imageSource}></img>
+            </li>
+          ),
+        )}
+      </ul>
+    </div>
+  );
 };
